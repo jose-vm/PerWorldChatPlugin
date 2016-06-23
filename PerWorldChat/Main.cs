c113021a22cb85cb5169fc64447c6df57f037af2
Jose Vazquez <cabo.rambo@hotmail.com>
2016-06-23 12:11:05 -0500

    Initial commit

---
+++ PerWorldChat/Main.cs
@@ -0,0 +1,26 @@
+﻿using MiNET;
+using MiNET.Net;
+using MiNET.Plugins;
+using MiNET.Plugins.Attributes;
+using MiNET.Utils;
+
+namespace PerWorldChat
+{
+    public class Main : Plugin
+    {
+        [PacketHandler, Receive]
+        public Package OnMessageSent(McpeText message, Player player)
+        {
+            string text = message.message;
+
+            if (text.StartsWith("/") || text.StartsWith("."))
+            {
+                return message;
+            }
+
+            text = TextUtils.RemoveFormatting(text);
+            player.Level.BroadcastMessage(text, type: MessageType.Raw);
+            return null;
+        }
+    }
+}
